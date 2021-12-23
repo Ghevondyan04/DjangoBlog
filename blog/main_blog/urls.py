0383a08b6aa72e8430d07d5c75699627f85a3f06
Hamlet <ghevondyan04@bk.ru>
2021-12-23 20:23:45 +0400

    second commit

---
+++ blog/main_blog/urls.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from django.urls import path
+from .views import HomeView, PostDetailView
+
+urlpatterns = [
+    path('', HomeView.as_view(), name='home'),
+    path('post/<int:pk>', PostDetailView.as_view(), name='post-detail'),
+]
---
+++ blog/main_blog/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+from .models import Post
+
+
+class HomeView(ListView):
+    model = Post
+    template_name = "index.html"
+
+
+class PostDetailView(DetailView):
+    model = Post
+    template_name = "post_detail.html"
